0feaeb0fd606376eee0a39995a5dadc93cf78bdf
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2021-10-02 19:56:45 +0600

    Updated testimonial

--- src/components/Pages/HomePage/Testimonial.jsx
+++ src/components/Pages/HomePage/Testimonial.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import shovonPhoto from "../../../assets/photos/shovon.jpeg"
-import billGatesPhoto from "../../../assets/photos/billgates.jpeg"
-import sakibPhoto from "../../../assets/photos/sakibalhasan.jpeg"
+import sakibmollick from "../../../assets/photos/sakibmollick.jpeg"
+import nusratjahankeya from "../../../assets/photos/Nusrat-Jahan-Keya.jpeg"
 
 function Testimonial() {
     return (
@@ -22,22 +22,22 @@ function Testimonial() {
                     </div>
                     <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                         <div className="h-full text-center">
-                            <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={billGatesPhoto} />
+                            <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={nusratjahankeya} />
                             <p className="leading-relaxed">The best ever platform to learn English in Bangladesh. I really appreciate the effort which gives us a lot of support while learning English.
 </p>
                             <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
-                            <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">William Henry Gates</h2>
-                            <p className="text-gray-500">Co-Founder Of Microsoft</p>
+                            <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Nusrat Jahan Keya</h2>
+                            <p className="text-gray-500">Student Of Class 8</p>
                         </div>
                     </div>
                     <div className="lg:w-1/3 lg:mb-0 p-4">
                         <div className="h-full text-center">
-                            <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={sakibPhoto} />
-                            <p className="leading-relaxed">The best ever platform to learn English in Bangladesh. I really appreciate the effort which gives us a lot of support while learning English.
+                            <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={sakibmollick} />
+                            <p className="leading-relaxed">I supposed to think English just a subject like math or physics. But this platform gave me the 
 </p>
                             <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
-                            <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Sakib Al Hasan</h2>
-                            <p className="text-gray-500">Bangladeshi Cricket All-Rounder</p>
+                            <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">MD. Sakib Mollick</h2>
+                            <p className="text-gray-500">Student Of Class 10</p>
                         </div>
                     </div>
                 </div>
